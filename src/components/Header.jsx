45d2b5e26d73f128d25270cb2a90301238d5ccef
emaburgos <emaburgos17@hotmail.com>
2023-03-05 02:43:28 -0300

    Repo de proyecto React

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+    return(
+        <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+        ¿Cuánto <span className="text-lime-500">dinero</span> necesitas?
+        </h1>
+
+    )
+}
+
+export default Header;
---
+++ src/helpers/index.js
@@ -0,0 +1,41 @@
+const formatearDinero = (valor) => {
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    });
+    return formatter.format(valor)
+}
+
+
+
+const calcularTotalPagar = (cantidad, plazo) => {
+    let total;
+
+    if (cantidad < 5000) {
+        total = cantidad * 1.5;
+        
+    } else if(cantidad >= 5000 && cantidad < 10000) {
+        total = cantidad * 1.4;
+
+    } else if(cantidad >= 10000 && cantidad < 15000) {
+        total = cantidad * 1.3;
+
+    } else {
+        total = cantidad * 1.2;
+
+    }
+    if(plazo === 6) {
+        total *= 1.1;
+    } else if(plazo === 12) {
+        total *= 1.2;
+    } else {
+        total *= 1.3;
+        
+
+    } return total
+}
+
+export {
+    formatearDinero,
+    calcularTotalPagar
+}
